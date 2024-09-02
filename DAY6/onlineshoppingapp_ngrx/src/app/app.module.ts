f9de2352748ef0e4477a434ccfa3da80afa90b93
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-09-02 13:15:25 +0530

    ✨ OnlineSHopping App - NGRX - Bootstrap

--- DAY6/onlineshoppingapp_ngrx/src/app/app.module.ts
+++ DAY6/onlineshoppingapp_ngrx/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { productsReducer } from 'src/ngrx/reducers/products.reducer';
 import { ListofproductsComponent } from './components/listofproducts/listofproducts.component';
+import { ProductComponent } from './components/product/product.component';
+import { IconComponent } from './components/atoms/icon/icon.component';
+import { RatingComponent } from './components/molecules/rating/rating.component';
 
 @NgModule({
-  declarations: [AppComponent, ListofproductsComponent],
+  declarations: [AppComponent, ListofproductsComponent, ProductComponent,IconComponent,RatingComponent],
   imports: [BrowserModule, StoreModule.forRoot({ products: productsReducer })],
   providers: [],
   bootstrap: [AppComponent],
