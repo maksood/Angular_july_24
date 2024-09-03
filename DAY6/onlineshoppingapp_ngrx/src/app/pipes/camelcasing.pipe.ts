10a1f5114cbce4941020bd9e87fd53ada21f6a82
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-09-03 13:09:12 +0530

    ✨ Passing Pipes arguments

--- DAY6/onlineshoppingapp_ngrx/src/app/pipes/camelcasing.pipe.ts
+++ DAY6/onlineshoppingapp_ngrx/src/app/pipes/camelcasing.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'camelcase' })
 export class CamelCasePipe implements PipeTransform {
   transform(value: any, ...args: any[]) {
+    console.log(args);
     if (typeof value !== 'string') {
       return value;
     }
